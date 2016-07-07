4adec0f172f75a6ede9e27efba65cbf8de829d91
tolyasjar <toleenj@gmail.com>
2016-07-07 11:34:13 -0500

    Initial Commit

---
+++ Autolayouts_&_StackViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Autolayouts_&_StackViews
+//
+//  Created by Toleen Jaradat on 7/7/16.
+//  Copyright © 2016 Toleen Jaradat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
